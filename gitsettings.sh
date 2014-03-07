1499453d153539314e8bff2e9808011bc8935a01
David Hooey <davidhooey@gmail.com>
2014-03-07 07:45:54 -0500

    Adding p4merge as git difftool.

--- gitsettings.sh
+++ gitsettings.sh
@@ -7,3 +7,7 @@ git config --global alias.a add
 git config --global alias.c commit
 git config --global alias.l "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) - %C(blue)%an <%ae>%Creset' --abbrev-commit"
 
+git config --global diff.tool p4merge
+git config --global difftool.p4merge.cmd '/Applications/p4merge.app/Contents/MacOS/p4merge $LOCAL $REMOTE'
+git config --global difftool.prompt false
+
